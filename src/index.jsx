3b997cd430e64c0e7ad22fa1b60e0c7c4f195775
Maurice (Jaid) <mail@j4id.com>
2017-10-13 19:22:33 +0200

    Implemented basic authorization and /users queries

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,123 @@
+import EventEmitter from "events"
+import lodash from "lodash"
 import request from "request"
 
 export default class TwitchHelix {
 
     constructor(options) {
-
+        if (lodash.isEmpty(options)) {
+            throw new Error("TwitchHelix constructor needs options object as first argument")
+        }
+        const requiredOptions = ["clientId", "clientSecret"]
+        for (const requiredOption of requiredOptions) {
+            if (!options[requiredOption]) {
+                throw new Error(`Required TwitchHelix option ${requiredOption} is ${options[requiredOption]}`)
+            }
+        }
+        this.options = Object.assign(options, {
+            prematureExpirationTime: 10000,
+            autoAuthorize: true
+        })
+        this.accessToken = null
+        this.refreshToken = null
+        this.tokenExpiration = null
+        this.eventEmitter = new EventEmitter()
     }
 
-    getUserByName(username) {
+    updateApiRequest() {
+        this.apiRequest = request.defaults({
+            baseUrl: "https://api.twitch.tv/helix",
+            jar: true,
+            json: true,
+            gzip: true,
+            headers: {
+                Authorization: `Bearer ${this.accessToken}`
+            }
+        })
+    }
 
+    on(type, handler) {
+        this.eventEmitter.on(type, handler)
+    }
+
+    log(level, message) {
+        this.eventEmitter.emit("log-" + level, message)
+    }
+
+    authorize() {
+        return new Promise((resolve, reject) => {
+            request.post("https://api.twitch.tv/kraken/oauth2/token", {
+                qs: {
+                    client_id: this.options.clientId,
+                    client_secret: this.options.clientSecret,
+                    grant_type: "client_credentials"
+                },
+                gzip: true,
+                json: true
+            }, (error, response, body) => {
+                if (error) {
+                    reject(error)
+                    return
+                }
+                this.accessToken = body.access_token
+                this.refreshToken = body.refresh_token
+                this.tokenExpiration = Date.now() + (body.expires_in * 1000)
+                this.updateApiRequest()
+                resolve(this.tokenExpiration)
+            })
+        })
+    }
+
+    isAuthorized() {
+        return this.accessToken && (Date.now() - this.options.prematureExpirationTime > this.tokenExpiration)
+    }
+
+    async autoAuthorize() {
+        if (!this.isAuthorized() && this.options.autoAuthorize) {
+            await this.authorize()
+        }
+    }
+
+    sendApiRequest(query) {
+        return new Promise(async (resolve, reject) => {
+            await this.autoAuthorize()
+            this.apiRequest.get(query, (error, response, body) => {
+                if (error) {
+                    reject(error)
+                    return
+                }
+                if (!body) {
+                    const errorMessage = `Got an invalid response body from Twitch API: ${body}`
+                    this.log("error", errorMessage)
+                    reject(errorMessage)
+                    return
+                }
+                if (!body.data) {
+                    const errorMessage = `Got invalid response data from Twitch API: ${body}`
+                    this.log("error", errorMessage)
+                    reject(errorMessage)
+                    return
+                }
+                resolve(body.data)
+            })
+        })
+    }
+
+    async getTwitchUserByName(username) {
+        const data = await this.sendApiRequest(`users?login=${username}`)
+        return data[0]
+    }
+
+    async getTwitchUsersByName(usernames) {
+        if (!usernames || !usernames.length) {
+            return []
+        }
+        if (usernames.length > 100) {
+            this.log("warn", `Tried to retrieve data from Twitch API for more ${usernames.length} usernames at once! Using the first 100 usernames and discarding ${usernames.length} usernames`)
+            usernames.length = 100
+        }
+        const data = await this.sendApiRequest("users?login=" + usernames.join("&login="))
+        return data
     }
 
 }
--- tests/index.test.jsx
+++ tests/index.test.jsx
@@ -1,5 +1,32 @@
 import TwitchHelix from "../src/index"
 
-test("TwitchHelix functions should return legit values", () => {
-    new TwitchHelix()
+require("dotenv").config()
+
+const clientId = process.env.TWITCH_CLIENT_ID
+const clientSecret = process.env.TWITCH_CLIENT_SECRET
+
+test("TwitchHelix functions should return legit values", async () => {
+    const twitchClient = new TwitchHelix({clientId, clientSecret})
+    expect(typeof twitchClient).toBe("object")
+    const tokenExpiration = await twitchClient.authorize()
+    expect(tokenExpiration).toBeGreaterThan(Date.now())
+    const j4idn = await twitchClient.getTwitchUserByName("j4idn")
+    expect(j4idn.description).toMatch("")
+    expect(j4idn.display_name).toMatch("")
+    expect(j4idn.id).toBe("65887522")
+    expect(j4idn.login).toBe("j4idn")
+    expect(j4idn.offline_image_url).toMatch("https://")
+    expect(j4idn.profile_image_url).toMatch("https://")
+    expect(j4idn.view_count).toBeGreaterThan(0)
+    const bots = await twitchClient.getTwitchUsersByName(["moobot", "nightbot"])
+    expect(bots).toHaveLength(2)
+    const moobot = bots.find(twitchUser => twitchUser.login === "moobot")
+    const nightbot = bots.find(twitchUser => twitchUser.login === "nightbot")
+    expect(moobot.id).toBe("1564983")
+    expect(nightbot.id).toBe("19264788")
+})
+
+test("TwitchHelix should throw an Error if incorrectly constructed", () => {
+    expect(() => new TwitchHelix()).toThrow("needs options object")
+    expect(() => new TwitchHelix({clientId}).toThrow("TwitchHelix option clientSecret"))
 })
